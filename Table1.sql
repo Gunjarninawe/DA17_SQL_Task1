5b959f0dce845b4f1b17126632e08c00aa9c87ba
Gunjarninawe <gunjarninawe2808@gmail.com>
2025-03-29 23:16:21 +0530

    created a table in SQL

---
+++ Table1.sql
@@ -0,0 +1,53 @@
+create table Employee  (
+
+id bigint,
+name varchar,
+Department varchar,
+position varchar,
+salary bigint
+)
+
+select * from Employee data
+
+insert into Employee (id,name,Department,position,salary) values(1,'Alice Johson','IT','Developer',75000)
+insert into Employee (id,name,Department,position,salary) values(2,'Bob','HR','HR manager',60000)
+insert into Employee (id,name,Department,position,salary) values(3,'carol brown','finance','analyst',62000)
+insert into Employee (id,name,Department,position,salary) values(4,'David lee','marketing','content writer',74000)
+insert into Employee (id,name,Department,position,salary) values(5,'emily wilson','sales','sales manager',70000)
+insert into Employee (id,name,Department,position,salary) values(6,'frank harris','IT','Developer',85000)
+insert into Employee (id,name,Department,position,salary) values(7,'Grace Adams','HR','HR Manager',61000)
+insert into Employee (id,name,Department,position,salary) values(8,'Henry Moore','Finance','Analyst',63000)
+insert into Employee (id,name,Department,position,salary) values(9,'Irene Clark','Marketing','Content Writer',56000)
+insert into Employee (id,name,Department,position,salary) values(10,'Jack Lewis','Sales','Sales Manager',81000)
+insert into Employee (id,name,Department,position,salary) values(11	,'Karen Taylor'	,'IT','Developer',77000)
+insert into Employee (id,name,Department,position,salary) values(12,'Luke Thomas','HR'	,'HR Manager',70000)
+insert into Employee (id,name,Department,position,salary) values(13	,'Molly White','Finance',	'Analyst',64000)
+insert into Employee (id,name,Department,position,salary) values(14	,'Nick Scott','Marketing','ContentWriter',57000)
+insert into Employee (id,name,Department,position,salary) values(15	,'Olivia Green','Sales','Sales Manager',82000)
+insert into Employee (id,name,Department,position,salary) values(16	,'Paul Young',	'IT','Developer',78000)
+insert into Employee (id,name,Department,position,salary) values(17	,'Quinn Baker',	'HR','HR Manager',71000)
+insert into Employee (id,name,Department,position,salary) values(18,'Rachel Hall',	'Finance','Analyst',65000)
+insert into Employee (id,name,Department,position,salary) values(19,'Sam Allen','Marketing'	,'ContentWriter',58000)
+insert into Employee (id,name,Department,position,salary) values(20,'Tina Wright','Sales','Sales Manager',83000)
+insert into Employee (id,name,Department,position,salary) values(21,'Uma Carter','IT','Developer',79000)
+insert into Employee (id,name,Department,position,salary) values(22,'Victor Evans','HR','HR Manager',72000)
+insert into Employee (id,name,Department,position,salary) values(23,'Wendy Turner'	,'Finance','Analyst',66000)
+insert into Employee (id,name,Department,position,salary) values(24,'Xavier Morris','Marketing','Content Writer',	59000)
+insert into Employee (id,name,Department,position,salary) values(25,'Yara Bennett'	,'Sales','Sales Manager',84000)
+insert into Employee (id,name,Department,position,salary) values(26,'Zack Phillips','IT','Developer'	,80000)
+insert into Employee (id,name,Department,position,salary) values(27,'Abby Hughes','HR','HR Manager',73000)
+insert into Employee (id,name,Department,position,salary) values(28,'Brad Foster','Finance,'Analyst',67000)
+insert into Employee (id,name,Department,position,salary) values(29,'Cindy Ward','Marketing','Content Writer',60000)
+insert into Employee (id,name,Department,position,salary) values(30,'Derek James'	,'Sales','Sales Manager',85000)
+insert into Employee (id,name,Department,position,salary) values(31,Ella Cooper','IT','Developer'	81000)
+insert into Employee (id,name,Department,position,salary) values(32,'Fred Watson','HR','HR Manager',74000)
+insert into Employee (id,name,Department,position,salary) values(33,'Gina Brooks','Finance','Analyst',68000)
+insert into Employee (id,name,Department,position,salary) values(34,'Harry Rogers','Marketing','ContentWriter',61000)
+insert into Employee (id,name,Department,position,salary) values(35,'Isla Reed','Sales'	,'Sales Manager',86000)
+insert into Employee (id,name,Department,position,salary) values(36,'Jack Kelly','IT','Developer',82000)
+insert into Employee (id,name,Department,position,salary) values(37,'Kathy Price','HR','HR Manager',75000)
+insert into Employee (id,name,Department,position,salary) values(38,'Liam Bailey','Finance','Analyst',69000)
+insert into Employee (id,name,Department,position,salary) values(39,'Mia Gray','Marketing','ContentWriter',62000)
+insert into Employee (id,name,Department,position,salary) values(40,'Noah Powell','Sales','Sales Manager',87000)
+
+
